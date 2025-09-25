00817dd02500185c1207a0794da1a067f48738f1
Aneka16 <aneka16.srivastava@gmail.com>
2025-09-25 10:10:55 +0530

    thread 25/09/25

--- src/Unit2/Thread1.java
+++ src/Unit2/Thread1.java
@@ -1,11 +1,15 @@
 package Unit2;
 //ye child thread hai we need to make Main thread
 //1 se 10 tak number print krna hai
-public class Thread1 extends Thread
-{
-    public void run(){
-        for(int i=1;i<11;i++){
-            System.out.println("thread 1 "+i);
+public class Thread1 extends Thread {
+    public void run() {
+        try {
+            for (int i = 1; i < 11; i++) {
+                System.out.println("thread 1 " + i);
+                Thread.sleep(10000);//har output 10 10 sec ke interval mei aayega
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
